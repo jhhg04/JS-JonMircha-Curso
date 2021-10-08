2d94924960a0710509681705f0dd3f9f6e0ee225
jhhg04 <jhhg04@gmail.com>
2021-10-08 07:11:38 -0500

    add

--- 33-modulosImportExport/js/modulos.js
+++ 33-modulosImportExport/js/modulos.js
@@ -1,5 +1,11 @@
-import { PI } from './constantes';
+import saludar, { Saludar, PI, usuario } from './constantes.js';
+import { aritmetica as calculos } from './aritmetica.js';
 
-console.log('Archivos modulos.js');
-
-console.log(PI);
+console.log('Archivo modulos.js');
+console.log(PI, usuario);
+//console.log(aritmetica.sumar(3, 4));
+console.log(calculos.sumar(3, 4));
+console.log(calculos.restar(3, 4));
+saludar();
+let saludo = new Saludar();
+saludo;
